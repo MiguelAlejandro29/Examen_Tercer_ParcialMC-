04192aeae7454f98fc78f7261c92f92f60823ea6
MiguelAlejandro29 <cardenas29mc@gmail.com>
2022-04-25 23:49:38 -0600

    Examen 3 actualización 1
    Examen parcial 3 actualización 1

---
+++ Examen Tercer Parcial/Examen Parcial 3/Controllers/LoginController.cs
@@ -0,0 +1,80 @@
+﻿using Datos1.Interfaces;
+using Datos1.Repositorios;
+using Examen_Parcial_3.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Modelo;
+using MySql.Data.MySqlClient;
+using System.Security.Claims;
+
+namespace Examen_Parcial_3.Controllers;
+
+public class LoginController : Controller
+{
+    private readonly MySqlConfiguration _configuration;
+    private IUsuarioRepositorio _usuarioRepositorio;
+
+    public LoginController(MySQLConfiguration configuration)
+    {
+        _configuration = configuration;
+        _usuarioRepositorio = new UsuarioRepositorio(configuration.CadenaConexion);
+    }
+
+    [HttpPost("/account/login")]
+    public async Task<IActionResult> Login(Login login)
+    {
+        string rol = string.Empty;
+        try
+        {
+            bool usuarioValido = await _usuarioRepositorio.ValidaUsuario(login);
+            if (usuarioValido)
+            {
+                Usuario usu = await _usuarioRepositorio.GetPorCodigo(login.Codigo);
+                if (usu.EstaActivo)
+                {
+                    rol = usu.Rol;
+
+                    //Añadimos los claims Usuario y Rol para tenerlos disponibles en la Cookie
+                    var claims = new[]
+                    {
+                        new Claim(ClaimTypes.Name, usu.Codigo),
+                        new Claim(ClaimTypes.Role, rol)
+                    };
+
+                    //Creamos el principal
+                    var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                    //Generamos la cookie. SignInAsync es un método de extensión del contexto.
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal,
+                                                new AuthenticationProperties
+                                                {
+                                                    IsPersistent = true,
+                                                    ExpiresUtc = DateTime.UtcNow.AddMinutes(20)
+                                                });
+                }
+                else
+                {
+                    return LocalRedirect("/login/Usuario Inactivo");
+                }
+            }
+            else
+            {
+                return LocalRedirect("/login/Datos de usuario Invalido");
+            }
+        }
+        catch (Exception ex)
+        {
+            return LocalRedirect("/login/Datos de usuario Invalido");
+        }
+        return LocalRedirect("/");
+    }
+
+    [HttpGet("/account/logout")]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return LocalRedirect("/");
+    }
+}
